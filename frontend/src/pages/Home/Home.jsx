9cd6165e2c2049fe9e5e5696019615c08d685396
VarnikaGoel2004 <vanu2004goel@gmail.com>
2025-04-06 13:18:23 +0530

    Home page created

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -1,11 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './home.css';
 import Header from '../../components/Header/header';
+import ExploreMenu from '../../components/ExploreMenu/ExploreMenu';
+import FoodDisplay from '../../components/FoodDisplay/FoodDisplay';
 
 const Home = () => {
+   const [category,setCategory] = useState("All");
+
   return (
     <div className="home">
       <Header />
+      <ExploreMenu category={category} setCategory={setCategory}/>
+      <FoodDisplay category={category} />
     </div>
   );
 };
