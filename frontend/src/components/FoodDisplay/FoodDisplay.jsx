bf9c52e3442547f72057976c0716ab8631ed802b
VarnikaGoel2004 <vanu2004goel@gmail.com>
2025-04-07 13:13:43 +0530

    Add to cart and remove to cart funcionality added and footer section created.

--- frontend/src/components/FoodDisplay/FoodDisplay.jsx
+++ frontend/src/components/FoodDisplay/FoodDisplay.jsx
@@ -1,20 +1,32 @@
-import React, { use, useContext } from 'react'
-import './FoodDisplay.css'
-import { StoreContext } from '../../context/StoreContext'
-import FoodItem from '../FoodItem/FoodItem'
+import React, { use, useContext } from "react";
+import "./FoodDisplay.css";
+import { StoreContext } from "../../context/StoreContext";
+import FoodItem from "../FoodItem/FoodItem";
 
-const FoodDisplay = ({category}) => {
-    const {food_list} = useContext(StoreContext)
+const FoodDisplay = ({ category }) => {
+  const { food_list } = useContext(StoreContext);
   return (
-    <div className='food-display' id = 'food-display'>
-        <h2>Top dishes near you.</h2>
-        <div className="food-display-list">
-            {food_list.map((item,index)=>{
-                return <FoodItem key = {index} id= {item.id} name={item.name} description={item.description} price = {item.price} image = {item.image}/>
-            })}
-        </div>
+    <div className="food-display" id="food-display">
+      <h2>Top dishes near you.</h2>
+      <div className="food-display-list">
+        {food_list.map((item) => {
+          if(category==='All' ||category===item.category){
+            return (
+              <FoodItem
+                key={item.id} // ✅ use unique key
+                id={item.id} // ✅ make sure this is defined
+                name={item.name}
+                description={item.description}
+                price={item.price}
+                image={item.image}
+              />
+            );
+          }
+         
+        })}
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default FoodDisplay
+export default FoodDisplay;
